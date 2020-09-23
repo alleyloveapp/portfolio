578e097bc8dc0c6d375e76dffa31dd12e8b68802
alleyloveapp <alleyloveapp@gmai.com>
2020-09-23 18:54:59 +0900

    app
    app

---
+++ viewer/story.js
@@ -0,0 +1,432 @@
+var story = {
+"docName": "921-protfolio-app-",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"serverToolsPath":"",
+"fileType":"png",
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"id" : "87426BD4-1F12-4283-8502-383A3AEC03C6",
+"index": 0,
+"image": "welcomepage.png",
+"image2x": "welcomepage@2x.png",
+"width": 414,
+"height": 834,
+"title": "Welcomepage",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 313,
+			"y": 707,
+			"width": 43,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 125,
+			"y": 591,
+			"width": 119,
+			"height": 19
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "9C7DD176-AA8B-40BF-9427-E31BBF926EE4",
+"index": 1,
+"image": "about_me.png",
+"image2x": "about_me@2x.png",
+"width": 414,
+"height": 834,
+"title": "About-me",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 339.5,
+			"y": 88.25,
+			"width": 25,
+			"height": 17.5
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "3AE52F58-5507-41A6-AB3E-DB190059661A",
+"index": 2,
+"image": "menu_bar.png",
+"image2x": "menu_bar@2x.png",
+"width": 414,
+"height": 834,
+"title": "menu-bar",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 197,
+			"y": 412,
+			"width": 119,
+			"height": 19
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 197,
+			"y": 378,
+			"width": 74,
+			"height": 19
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 115,
+			"y": 265,
+			"width": 185,
+			"height": 75
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "36BD8903-2A2F-48E0-BBF5-DB8A56CB0AB7",
+"index": 3,
+"image": "weebsite_design_1.png",
+"image2x": "weebsite_design_1@2x.png",
+"width": 414,
+"height": 834,
+"title": "Weebsite design-1",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 191,
+			"y": 593,
+			"width": 33,
+			"height": 33
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 339.5,
+			"y": 88.25,
+			"width": 25,
+			"height": 18
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "8A61DB9D-C1E6-46FA-A193-152A1933520A",
+"index": 4,
+"image": "weebsite_design_2.png",
+"image2x": "weebsite_design_2@2x.png",
+"width": 414,
+"height": 834,
+"title": "Weebsite design-2",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 191,
+			"y": 593,
+			"width": 33,
+			"height": 33
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 339.5,
+			"y": 88.25,
+			"width": 25,
+			"height": 18
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "CB96BD02-F04B-4838-B1D5-F77557AFBDF1",
+"index": 5,
+"image": "weebsite_design_3.png",
+"image2x": "weebsite_design_3@2x.png",
+"width": 414,
+"height": 834,
+"title": "Weebsite design-3",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 191,
+			"y": 725,
+			"width": 33,
+			"height": 33
+		},
+		"isParentFixed": false,
+		"page": 6,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 339.5,
+			"y": 88.25,
+			"width": 25,
+			"height": 18
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "A628FD1E-A1F5-46F5-B507-B06D609B0EE3",
+"index": 6,
+"image": "weebsite_design_4.png",
+"image2x": "weebsite_design_4@2x.png",
+"width": 414,
+"height": 834,
+"title": "Weebsite design-4",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 191,
+			"y": 725.0000000000001,
+			"width": 33,
+			"height": 33
+		},
+		"isParentFixed": false,
+		"page": 7,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 339.5,
+			"y": 88.25,
+			"width": 25,
+			"height": 18
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "AFACC503-3D5F-4BBC-9516-B8D7D127F685",
+"index": 7,
+"image": "weebsite_design_5.png",
+"image2x": "weebsite_design_5@2x.png",
+"width": 414,
+"height": 834,
+"title": "Weebsite design-5",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 339.5,
+			"y": 88.25,
+			"width": 25,
+			"height": 18
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"id" : "9F4F7F3A-E7B2-4BBB-9F09-3D3DA4363635",
+"index": 8,
+"image": "welcomepage_1.png",
+"image2x": "welcomepage_1@2x.png",
+"width": 414,
+"height": 834,
+"title": "Welcomepage-1",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"id" : "BF8BB1D5-FF17-4821-9421-976520950920",
+"index": 9,
+"image": "welcomepage_2.png",
+"image2x": "welcomepage_2@2x.png",
+"width": 414,
+"height": 834,
+"title": "Welcomepage-2",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"id" : "2E0404FB-2EB9-4F41-BC63-B4A4496265BC",
+"index": 10,
+"image": "welcomepage_4.png",
+"image2x": "welcomepage_4@2x.png",
+"width": 414,
+"height": 834,
+"title": "Welcomepage-4",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"id" : "6BEA7FC6-D3C0-4766-A9CA-994F3633A90C",
+"index": 11,
+"image": "welcomepage_3.png",
+"image2x": "welcomepage_3@2x.png",
+"width": 414,
+"height": 834,
+"title": "Welcomepage-3",
+'transAnimType': 0,
+'layout' : {
+	"offset": 0,
+	"totalWidth": 960,
+	"numberOfColumns": 12,
+	"columnWidth": 60,
+	"gutterWidth": 20
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "921-protfolio-app ",
+"startPageIndex": 7,
+"layersExist": true,
+"centerContent":  false,
+"totalImages": 12,
+"highlightLinks": false
+}
